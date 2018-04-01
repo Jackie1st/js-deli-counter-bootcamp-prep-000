7b7085b2003c622cd9e6803b928f72e4a633ea87
Jackie First <jackiejeep@gmail.com>
2018-04-01 19:52:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function currentLine(katzDeli) {
   if (line.length === 0) {
     return ("The line is currently empty.");
   } else {
-    return `The line is currently: ${line}`;
+    return `The line is currently:${line}`;
   }
 }
 
